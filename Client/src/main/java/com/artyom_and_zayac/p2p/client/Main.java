7c0412135abbc4dd89c47011c5284bbfad5d5d7e
artyom kabakov <matroskin7@i.ua>
2020-08-15 21:04:41 +0300

    Initial commit.

---
+++ Client/src/main/java/com/artyom_and_zayac/p2p/client/Main.java
@@ -0,0 +1,7 @@
+package com.artyom_and_zayac.p2p.client;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Server/src/main/java/com/artyom_and_zayac/p2p/server/Main.java
@@ -0,0 +1,6 @@
+package com.artyom_and_zayac.p2p.server;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
