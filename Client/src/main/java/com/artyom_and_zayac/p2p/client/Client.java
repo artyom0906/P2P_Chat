65dbacca7e9b2285cdd2c0597945db05189b7801
Zayac-The-Engineer <fed4chuck@gmail.com>
2020-08-15 23:09:21 +0300

    Now able to receive ip from server

--- Client/src/main/java/com/artyom_and_zayac/p2p/client/Client.java
+++ Client/src/main/java/com/artyom_and_zayac/p2p/client/Client.java
@@ -4,13 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.*;
+import java.util.Scanner;
 
 public class Client implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(Client.class);
     private DatagramSocket socket;
     private InetAddress address;
+    private Scanner scan = new Scanner(System.in);
 
     public Client() {
+        logger.info("Running constructor");
         try {
             socket = new DatagramSocket();
         } catch (SocketException e) {
@@ -27,14 +30,23 @@ public class Client implements Runnable {
 
     @Override
     public void run() {
-        String msg = "Hello";
-        try {
-            DatagramPacket packet
-                    = new DatagramPacket(msg.getBytes(), msg.getBytes().length,
-                    address, 7000);
-            socket.send(packet);
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
+        while(true) {
+            String msg = scan.nextLine();
+            try {
+                DatagramPacket packet
+                        = new DatagramPacket(msg.getBytes(), msg.getBytes().length,
+                        address, 7000);
+                socket.send(packet);
+
+                byte[]buf = new byte[4096];
+                packet = new DatagramPacket(buf, buf.length);
+                socket.receive(packet);
+                logger.info("msg: " + packet.getLength() + ": " +new String(packet.getData(), 0, packet.getLength()));
+                socket.receive(packet);
+                logger.info("msg: " + packet.getLength() + ": " +new String(packet.getData(), 0, packet.getLength()));
+            } catch (Exception e) {
+                logger.error(e.getMessage(), e);
+            }
         }
     }
 }
