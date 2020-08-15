ba66846acf40702fc311ae4b48bf8762a73dd641
artyom kabakov <matroskin7@i.ua>
2020-08-15 23:12:24 +0300

    finish server

--- Server/src/main/java/com/artyom_and_zayac/p2p/server/Server.java
+++ Server/src/main/java/com/artyom_and_zayac/p2p/server/Server.java
@@ -6,23 +6,23 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Server implements Runnable{
     private static final Logger logger = LoggerFactory.getLogger(Server.class);
     private final DatagramSocket serverSocket;
-    private static final List<String> usersInfo = new ArrayList<>();
+    private static final HashSet usersInfo = new HashSet();
     public Server() throws IOException {
-        serverSocket = new DatagramSocket(7000);//213.200.50.96
-        logger.info("test");
-        System.out.println("test");
+        serverSocket = new DatagramSocket(7000);
         new Thread(this).start();
     }
     @Override
     public void run() {
         while (true){
             try {
-                byte[] buf = new byte[256];
+                byte[] buf = new byte[4096];
                 DatagramPacket packet = new DatagramPacket(buf, buf.length);
                 logger.info("wait for packet");
                 serverSocket.receive(packet);
@@ -30,16 +30,21 @@ public class Server implements Runnable{
                 InetAddress inetAddress = packet.getAddress();
                 int port = packet.getPort();
                 byte[] data = packet.getData();
-                logger.info("msg:"+new String(data));
-                String userInfo = String.format("[%s, %s]", inetAddress, port);
+                logger.info("msg: " + packet.getLength() + ": " +new String(packet.getData(), 0, packet.getLength()));
+                String userInfo = String.format("[%s, %s]", inetAddress.getHostAddress(), port);
                 usersInfo.add(userInfo);
+
                 String userInfoToSend = String.format("syncServer:%s", userInfo);
-                packet = new DatagramPacket(userInfoToSend.getBytes(), userInfoToSend.length(), inetAddress, port);
+                byte[] dataToSend = userInfoToSend.getBytes();
+                logger.info(new String(dataToSend));
+                packet = new DatagramPacket(dataToSend,dataToSend.length, inetAddress, port);
                 serverSocket.send(packet);
 
-                String usersInfoSend = usersInfo.toString();
+                String usersInfoToSend = usersInfo.toString();
+                dataToSend = usersInfoToSend.getBytes();
+                packet = new DatagramPacket(dataToSend, dataToSend.length, inetAddress, port);
+                serverSocket.send(packet);
 
-                packet = new DatagramPacket(userInfoToSend.getBytes(), userInfoToSend.length(), inetAddress, port);
 
             }catch (Exception e){
                 logger.error(e.getMessage(), e);
